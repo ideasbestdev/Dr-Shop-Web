5558e477cc7af4044549ed8b6b5361af6e27d996
Husein-Shaito <84982235+Husein-Shaito@users.noreply.github.com>
2022-09-09 19:01:35 +0300

    tej

--- components/icons/BurgerIcon.tsx
+++ components/icons/BurgerIcon.tsx
@@ -8,6 +8,5 @@ export function BurgerIcon({ color }: Props) {
         <svg xmlns="http://www.w3.org/2000/svg" width="22.16" height="13.583" viewBox="0 0 22.16 13.583">
             <path id="Path_156" data-name="Path 156" d="M3,8H23.16M6.1,13.792H20.059M3,19.583H23.16" transform="translate(-2 -7)" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
         </svg>
-
     )
 }
