c40f172b38a246e9b3db9e6231d7a9d30f555f0b
Husein-Shaito <84982235+Husein-Shaito@users.noreply.github.com>
2022-09-09 17:34:50 +0300

    a

--- components/icons/ArrowDownIcon.tsx
+++ components/icons/ArrowDownIcon.tsx
@@ -8,7 +8,5 @@ export function ArrowDownIcon({ color }: Props) {
         <svg xmlns="http://www.w3.org/2000/svg" width="16.325" height="8.869" viewBox="0 0 16.325 8.869">
             <path id="Path_57" data-name="Path 57" d="M-7460.584-19397.5l7.456,7.455,7.455-7.455" transform="translate(7461.292 19398.207)" fill="#fff" stroke={color} strokeLinejoin="bevel" strokeWidth="2" />
         </svg>
-
-
     )
 }
