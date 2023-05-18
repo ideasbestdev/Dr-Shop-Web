cadb98ef1c564c0dbf7dd5171dfa479eec8e9d78
Husein-Shaito <84982235+Husein-Shaito@users.noreply.github.com>
2023-05-18 19:56:04 +0300

    banner

--- components/Home/BannerSection.tsx
+++ components/Home/BannerSection.tsx
@@ -1,11 +1,44 @@
-import { LinkButtonStyle, TitleStyle, TopBannerSectionStyle } from "@/styledcomponents/index";
+import { LinkButtonStyle, TitleStyle } from "@/styledcomponents/index";
 import React from "react";
 import { AssetsImages } from '@/utils/index';
 import Image from 'next/image';
+import styled from "styled-components";
 
 export function BannerSection() {
+    const MyBannerSectionStyle = styled.section`
+    display: flex;
+    position: relative;
+    .content{
+        z-index: 1;
+        position: absolute;
+        padding: 0 4%;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        h1{
+            max-width: 470px;
+            color: #fff;
+        }
+        a{
+            color: #fff;
+            background-color: #89CA00;
+            width: 266px;
+            height: 70px;
+        }
+    }
+    .image_container{
+        width: 100%;
+        min-height: 550px;
+        img{
+            object-fit: cover;
+        }
+    }
+`
     return (
-        <TopBannerSectionStyle>
+        <MyBannerSectionStyle>
             <section className='content'>
                 <TitleStyle>Now You Can Have Your DOWNLOAD APP Done Safely</TitleStyle>
                 <LinkButtonStyle>
@@ -15,6 +48,6 @@ export function BannerSection() {
             <div className='image_container'>
                 <Image src={AssetsImages.homeBanner} alt="homeBanner" />
             </div>
-        </TopBannerSectionStyle>
+        </MyBannerSectionStyle>
     )
 }
